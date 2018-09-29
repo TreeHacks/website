638a206c1186712738569c3e53d575e25add9531
Andrew Kondrich <andrewk1@stanford.edu>
2018-09-29 14:04:01 -0700

    added sponsorship mailto ref

--- src/js/sponsors.jsx
+++ src/js/sponsors.jsx
@@ -4,7 +4,7 @@ function Sponsors() {
   return(
     <div id="sponsors" className="container">
       <h1 className="section-heading">2019 treehacks sponsors</h1>
-      <a href="" className="green-button">become a sponsor!</a>
+      <a href="mailto:hello@treehacks.com?Subject=Sponsorship%20Interest" className="green-button">become a sponsor!</a>
     </div>
   );
 }
