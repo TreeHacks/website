c8562aa0e6409c8c53aac0985b2ea936df3cea5a
Olivia <obrown917@gmail.com>
2018-10-27 19:07:16 -0700

    Add Partners section, add MLH to it

--- src/js/sponsors.jsx
+++ src/js/sponsors.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 function Sponsors() {
   return(
     <div id="sponsors" className="container">
-      <h1 className="section-heading">sponsors to be announced </h1>
+      <h1 className="section-heading" id="sub-heading">sponsors</h1>
+      <p>(to be announced)</p>
       <a href="mailto:hello@treehacks.com?Subject=Sponsorship%20Interest" className="green-button">become a sponsor!</a>
+      <h1 className="section-heading">partners</h1>
+      <a href="https://mlh.io/seasons/na-2019/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2019-season&utm_content=white">
+        <img className = "mlh-trust-badge" src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-grayscale.svg"
+        alt="Major League Hacking 2019 Hackathon Season">
+        </img>
+      </a>
     </div>
   );
 }
