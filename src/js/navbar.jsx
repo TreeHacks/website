bfad0e475add7d14b69c9dfeb1a945db6575255d
Ashwin Ramaswami <aramaswamis@gmail.com>
2018-10-29 20:38:37 -0700

    Smooth scrolling

--- src/js/navbar.jsx
+++ src/js/navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as logo from '../svg/logo.svg';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 function Navbar() {
   return(
@@ -13,11 +14,11 @@ function Navbar() {
           </a>
         </li>
         <div id="mobile-menu">
-          <li className="navbar-item"><a href="#home">home</a></li>
-          <li className="navbar-item"><a href="#verticals">verticals</a></li>
-          <li className="navbar-item"><a href="#about">about</a></li>
-          <li className="navbar-item"><a href="#faq">faq</a></li>
-          <li className="navbar-item"><a href="#sponsors">sponsors</a></li>
+          <li className="navbar-item"><AnchorLink offset='100' href="#home">home</AnchorLink></li>
+          <li className="navbar-item"><AnchorLink offset='100' href="#verticals">verticals</AnchorLink></li>
+          <li className="navbar-item"><AnchorLink offset='100' href="#about">about</AnchorLink></li>
+          <li className="navbar-item"><AnchorLink offset='100' href="#faq">faq</AnchorLink></li>
+          <li className="navbar-item"><AnchorLink offset='100' href="#sponsors">sponsors</AnchorLink></li>
           <a href="https://apply.treehacks.com"><li id="navbar-apply">apply now!</li></a>
         </div>
       </ul>
