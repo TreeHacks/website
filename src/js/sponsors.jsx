22b7feb2434b1e2ad57c2f69607b3001f2b2575a
Theo Patt <theo@theopatt.com>
2018-11-15 09:55:53 -0800

    sponsors: remove partners section

--- src/js/sponsors.jsx
+++ src/js/sponsors.jsx
@@ -6,12 +6,6 @@ function Sponsors() {
       <h1 className="section-heading" id="sub-heading">sponsors</h1>
       <p>(to be announced)</p>
       <a href="mailto:hello@treehacks.com?Subject=Sponsorship%20Interest" className="green-button">become a sponsor!</a>
-      <h1 className="section-heading">partners</h1>
-      <a href="https://mlh.io/seasons/na-2019/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2019-season&utm_content=white">
-        <img className = "mlh-trust-badge" src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-grayscale.svg"
-        alt="Major League Hacking 2019 Hackathon Season">
-        </img>
-      </a>
     </div>
   );
 }
