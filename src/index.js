90a8b15aa6df7142032232336a8fed46f26064bd
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-01-31 08:14:12 -0800

    feat: set up /slack and /volunteer redirects, fixes #7

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ import Projects from './js/projects.jsx';
 import FAQ from './js/faq.jsx';
 import Sponsors from './js/sponsors.jsx';
 import Footer from './js/footer.jsx';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import './favicons/favicons';
 import './index.scss';
 
-function App() {
+function Main() {
   return (
     <div>
       <Navbar />
@@ -26,6 +27,22 @@ function App() {
       <Sponsors />
       <Footer />
     </div>
+  );
+}
+
+function CustomRedirect({from, to}) {
+  return <Route exact path={from} component={() => { window.location = to; return null;} }/>
+}
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <CustomRedirect from="/slack" to="https://join.slack.com/t/treehacks-2019/shared_invite/enQtNTE5ODY0MDI2NjQ3LWI2YjhlYTU3OTE1ODcyMTdmZWYzYWVjY2NjNzQ2YTBiZDY5YWYwYzhkMDhlZTBmNmNmMzMzZWYxM2QwMTMxMmI" />
+        <CustomRedirect from="/volunteer" to="https://goo.gl/forms/pZURr2kMSKwuFar42" />
+        <Route path="/" component={Main} />
+      </Switch>
+    </Router>
   );
 }
 
