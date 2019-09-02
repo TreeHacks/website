82ddba3a77cb7f55ca4c7c5548d90a915543365e
Jainil Sutaria <jsutaria@stanford.edu>
2019-09-02 01:46:11 -0400

    Fix 2020 dates for marketing posts

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Navbar from './js/navbar.jsx';
-import Circuit from './js/circuit.jsx';
-import Home from './js/home.jsx';
-import Verticals from './js/verticals.jsx';
-import About from './js/about.jsx';
-import Projects from './js/projects.jsx';
-import FAQ from './js/faq.jsx';
-import Sponsors from './js/sponsors.jsx';
-import Footer from './js/footer.jsx';
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "./js/navbar.jsx";
+import Circuit from "./js/circuit.jsx";
+import Home from "./js/home.jsx";
+import Verticals from "./js/verticals.jsx";
+import About from "./js/about.jsx";
+import Projects from "./js/projects.jsx";
+import FAQ from "./js/faq.jsx";
+import Sponsors from "./js/sponsors.jsx";
+import Footer from "./js/footer.jsx";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
-import './favicons/favicons';
-import './index.scss';
+import "./favicons/favicons";
+import "./index.scss";
 
 function Main() {
   return (
@@ -30,16 +35,37 @@ function Main() {
   );
 }
 
-function CustomRedirect({from, to}) {
-  return <Route exact path={from} component={() => { window.location = to; return null;} }/>
+function CustomRedirect({ from, to }) {
+  return (
+    <Route
+      exact
+      path={from}
+      component={() => {
+        window.location = to;
+        return null;
+      }}
+    />
+  );
 }
 
 function App() {
   return (
     <Router>
       <Switch>
-        <CustomRedirect from="/slack" to="https://join.slack.com/t/treehacks-2019/shared_invite/enQtNTE5ODY0MDI2NjQ3LWI2YjhlYTU3OTE1ODcyMTdmZWYzYWVjY2NjNzQ2YTBiZDY5YWYwYzhkMDhlZTBmNmNmMzMzZWYxM2QwMTMxMmI" />
-        <CustomRedirect from="/volunteer" to="https://goo.gl/forms/pZURr2kMSKwuFar42" />
+        <CustomRedirect
+          from="/slack"
+          to="https://join.slack.com/t/treehacks-2019/shared_invite/enQtNTE5ODY0MDI2NjQ3LWI2YjhlYTU3OTE1ODcyMTdmZWYzYWVjY2NjNzQ2YTBiZDY5YWYwYzhkMDhlZTBmNmNmMzMzZWYxM2QwMTMxMmI"
+        />
+        <CustomRedirect
+          from="/volunteer"
+          to="https://goo.gl/forms/pZURr2kMSKwuFar42"
+        />
+        {/* 
+        ****For when team applications are open****
+        <CustomRedirect
+          from="/join"
+          to="https://treehacks.typeform.com/to/rK08ej"
+        /> */}
         <Route exact path="/" component={Main} />
         <Redirect to="/" />
       </Switch>
@@ -47,8 +73,4 @@ function App() {
   );
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/js/home.jsx
+++ src/js/home.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from '../svg/logo.svg';
-import { home, live_cutoff, ended_cutoff } from './content.json';
-import DeadlinesWidget from './deadlines-widget.jsx';
-import EmailSignupWidget from './email-signup-widget.jsx';
+import React from "react";
+import logo from "../svg/logo.svg";
+import { home, live_cutoff, ended_cutoff } from "./content.json";
+import DeadlinesWidget from "./deadlines-widget.jsx";
+import EmailSignupWidget from "./email-signup-widget.jsx";
 
 class Home extends React.Component {
   constructor(props) {
@@ -13,12 +13,19 @@ class Home extends React.Component {
   }
 
   componentDidMount() {
-    let width = window.innerWidth
-      || document.documentElement.clientWidth
-      || document.body.clientWidth;
+    let width =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
 
     if (width > 750) {
-      this.interval = setInterval(() => this.setState({phrase: (this.state.phrase + 1) % (home.length - 1)}), 7000);
+      this.interval = setInterval(
+        () =>
+          this.setState({
+            phrase: (this.state.phrase + 1) % (home.length - 1)
+          }),
+        7000
+      );
     }
   }
 
@@ -29,38 +36,46 @@ class Home extends React.Component {
   render() {
     const hasEnded = Date.now() > new Date(ended_cutoff),
       hasBegun = Date.now() > new Date(live_cutoff);
-    
-    return(
+
+    return (
       <div id="home">
-        <img src={logo} alt="treehacks large logo"/>
+        <img src={logo} alt="treehacks large logo" />
         <h1>
           <span className="logo-text-tree">tree</span>
           <span className="logo-text-hacks">hacks</span>
         </h1>
-        <h2>February 15-17, 2019 at&nbsp;Stanford&nbsp;University</h2>
-        {hasEnded ? <h2 className="ended">Thanks for a great 2019 &ndash; stay&nbsp;tuned&nbsp;for&nbsp;2020!</h2> : null}
+        <h2>February 14-16, 2020 at&nbsp;Stanford&nbsp;University</h2>
+        {/* {hasEnded ? <h2 className="ended">Thanks for a great 2019 &ndash; stay&nbsp;tuned&nbsp;for&nbsp;2020!</h2> : null} */}
         <div id="typewriter-container">
           <p>if ( </p>
-          <Typewriter text={home[this.state.phrase]}/>
+          <Typewriter text={home[this.state.phrase]} />
           <p> ) &#123;</p>
         </div>
-        {hasEnded ?
+        {hasEnded ? (
           <div>
             <EmailSignupWidget />
           </div>
-        : hasBegun ?
+        ) : hasBegun ? (
           <div>
-            <a href="https://live.treehacks.com" className="green-button">view live schedule</a>
-            <a href="https://root.treehacks.com" className="green-button subtle">go to dashboard</a>
+            <a href="https://live.treehacks.com" className="green-button">
+              view live schedule
+            </a>
+            <a
+              href="https://root.treehacks.com"
+              className="green-button subtle"
+            >
+              go to dashboard
+            </a>
           </div>
-        :
+        ) : (
           <div>
-            <a href="https://root.treehacks.com" className="green-button">Stanford students register&nbsp;now!</a>
+            <a href="https://root.treehacks.com" className="green-button">
+              Stanford students register&nbsp;now!
+            </a>
             <DeadlinesWidget />
           </div>
-        }
-        <div>
-        </div>
+        )}
+        <div></div>
         <p>&#125;</p>
       </div>
     );
@@ -73,21 +88,21 @@ class Typewriter extends React.Component {
     this.state = {
       class: "",
       text: props.text
-    }
+    };
   }
 
   componentDidMount() {
     this.timer = setTimeout(() => {
-      this.setState({class: " show"});
+      this.setState({ class: " show" });
       clearTimeout(this.timer);
     }, 2500);
   }
 
   componentDidUpdate(prevProps, type) {
     if (this.props.text !== prevProps.text) {
-      this.setState({class: ""});
+      this.setState({ class: "" });
       this.timer = setTimeout(() => {
-        this.setState({class: " show", text: this.props.text});
+        this.setState({ class: " show", text: this.props.text });
         clearTimeout(this.timer);
       }, 2500);
     }
