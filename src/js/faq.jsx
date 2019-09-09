a92ebe99c6ae6d8c1f6a3cadbe260b4d0e689efb
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-09-09 00:39:04 +0000

    add animation

--- src/js/faq.jsx
+++ src/js/faq.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {faq} from './content.json';
+import { faq } from './content.json';
 
 const plusList = [
   require("../svg/plus1.svg"),
@@ -11,17 +11,31 @@ const plusList = [
 ]
 
 class FAQ extends React.Component {
+  constructor(props) {
+    super(props);
+    let selected = {};
+    for (const i in plusList) {
+      selected[String(i)] = false;
+    }
+    this.state = {
+      selected
+    }
+  }
   render() {
-    return(
+    return (
       <div id="faq">
         <h1 className="section-heading">Any Questions?</h1>
         <div className="faq-container">
-          {faq.map(({q, a}, i) => <div className="faq-item">
-            <h2 className="section-subheading faq-question">
+          {faq.map(({ q, a }, i) => <div className="faq-item" key={q + a}>
+            <h2
+              className="section-subheading faq-question"
+              onClick={e => this.setState({ selected: { ...this.state.selected, [String(i)]: !this.state.selected[String(i)] } })}>
               <img className="faq-plus" src={plusList[i % plusList.length]} />
               {q}
             </h2>
-            <p className="faq-answer">{a}</p>
+            <p className="faq-answer"
+              style={{ maxHeight: this.state.selected[String(i)] ? 9999 : 0 }}
+            >{a}</p>
           </div>)}
         </div>
       </div>
