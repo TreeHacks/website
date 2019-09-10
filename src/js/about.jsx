ee600c27352870b2f49920caa6016dee95b6322c
Jainil Sutaria <jsutaria@stanford.edu>
2019-09-09 18:01:16 -0700

    Fix about

--- src/js/about.jsx
+++ src/js/about.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
-import {about_intro} from './content.json';
-import {about_body} from './content.json';
-
+import React from "react";
+import ReactPlayer from "react-player";
+import { about_intro } from "./content.json";
+import { about_body } from "./content.json";
 
 function About() {
   let videoURL = "https://www.youtube.com/embed/mSlJTPzSH6g";
-  return(
+  return (
     <div id="about">
       <h1 className="section-heading">About</h1>
       <div className="container">
-        <div><p id="intro">{about_intro}</p><p id="body">{about_body}</p></div>
-        <div><ReactPlayer url={videoURL} width="100%" height="100%" /></div>
+        <div>
+          <p id="intro">{about_intro}</p>
+          <p id="body">{about_body}</p>
+        </div>
+        <div>
+          <ReactPlayer url={videoURL} width="100%" height="100%" />
+        </div>
       </div>
     </div>
   );
