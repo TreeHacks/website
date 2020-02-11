816adf4d5b04dd07ec9ca11fd3bd44a341a384ca
Ankush Swarnakar <ankushswar1@gmail.com>
2020-02-10 20:41:02 -0800

    logo fix

--- src/js/sponsors.jsx
+++ src/js/sponsors.jsx
@@ -3,40 +3,40 @@ import computer from "../svg/computer-arms.svg"
 
 const SPONSORS = {
   biggest: [
-    {"img": require('../sponsor-logos/humancapital.png')}
+    {"link": "https://human.capital", "img": require('../sponsor-logos/humancapital.png')}
   ],
   big: [
     {"link": "https://microsoft.com", "img": require('../sponsor-logos/microsoft.png')},
-    {"img": require('../sponsor-logos/johnsoncontrols.png')},
-    {"img": require('../sponsor-logos/oracle.png')},
-    {"img": require('../sponsor-logos/genentech.png')}
+    {"link": "https://www.johnsoncontrols.com", "img": require('../sponsor-logos/johnsoncontrols.png')},
+    {"link": "https://www.oracle.com/index.html", "img": require('../sponsor-logos/oracle.png')},
+    {"link": "https://www.gene.com", "img": require('../sponsor-logos/genentech.png')}
   ],
   medium: [
-    {"img": require('../sponsor-logos/citadel.png')},
-    {"img": require('../sponsor-logos/deshaw.png')},
-    {"img": require('../sponsor-logos/drw.png')},
-    {"img": require('../sponsor-logos/gcp.png')},
-    {"img": require('../sponsor-logos/jpm.png')},
-    {"img": require('../sponsor-logos/alwaysai.png')},
-    {"img": require('../sponsor-logos/fujifilm.png')},
-    {"img": require('../sponsor-logos/goahead.png')}
+    {"link:": "https://www.citadel.com", "img": require('../sponsor-logos/citadel.png')},
+    {"link": "https://www.deshaw.com", "img": require('../sponsor-logos/deshaw.png')},
+    {"link": "https://drw.com", "img": require('../sponsor-logos/drw.png')},
+    {"link": "https://cloud.google.com", "img": require('../sponsor-logos/gcp.png')},
+    {"link": "https://www.jpmorgan.com", "img": require('../sponsor-logos/jpm.png')},
+    {"link": "https://alwaysai.co", "img": require('../sponsor-logos/alwaysai.png')},
+    {"link": "https://www.fujifilm.com", "img": require('../sponsor-logos/fujifilm.png')},
+    {"link": "http://www.goaheadvc.com", "img": require('../sponsor-logos/goahead.png')}
   ],
   small: [
-    {"img": require('../sponsor-logos/facebook.png')},
-    {"img": require('../sponsor-logos/snapchat.png')},
-    {"img": require('../sponsor-logos/mmi.png')},
-    {"img": require('../sponsor-logos/twosigma.png')},
-    {"img": require('../sponsor-logos/checkbook.png')},
-    {"img": require('../sponsor-logos/infosys.png')},
-    {"img": require('../sponsor-logos/sequoia.png')}
+    {"link": "https://about.fb.com", "img": require('../sponsor-logos/facebook.png')},
+    {"link": "https://snapchat.com", "img": require('../sponsor-logos/snapchat.png')},
+    {"link": "https://www.mapmyindia.com", "img": require('../sponsor-logos/mmi.png')},
+    {"link": "https://www.twosigma.com", "img": require('../sponsor-logos/twosigma.png')},
+    {"link": "https://checkbook.io", "img": require('../sponsor-logos/checkbook.png')},
+    {"link": "https://www.infosys.com", "img": require('../sponsor-logos/infosys.png')},
+    {"link": "https://www.sequoiacap.com", "img": require('../sponsor-logos/sequoia.png')}
   ],
   smallest: [
-    {"img": require('../sponsor-logos/nvidia.png')},
-    {"img": require('../sponsor-logos/linode.png')},
-    {"img": require('../sponsor-logos/pearvc.png')},
-    {"img": require('../sponsor-logos/soundhound.png')},
-    {"img": require('../sponsor-logos/vatic.png')},
-    {"img": require('../sponsor-logos/voiceflow.png')}
+    {"link": "https://www.nvidia.com", "img": require('../sponsor-logos/nvidia.png')},
+    {"link": "https://www.linode.com", "img": require('../sponsor-logos/linode.png')},
+    {"link": "https://www.pear.vc", "img": require('../sponsor-logos/pearvc.png')},
+    {"link": "https://www.soundhound.com", "img": require('../sponsor-logos/soundhound.png')},
+    {"link": "http://www.vaticlabs.ai", "img": require('../sponsor-logos/vatic.png')},
+    {"link": "https://www.voiceflow.com/", "img": require('../sponsor-logos/voiceflow.png')}
   ]
 };
 
