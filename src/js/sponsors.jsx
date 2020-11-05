fe807871f4fd8e520eb276ab9610f9f43912f245
marcopizarro <marcoinnovates@gmail.com>
2020-11-04 20:20:16 -0800

    remove stripes

--- src/js/sponsors.jsx
+++ src/js/sponsors.jsx
@@ -26,7 +26,7 @@ function Sponsors() {
   return (
     <div id="sponsors" className="container">
       <div className="stripe-wrapper sponsors">
-        <div className="stripe accent-gray" />
+        {/* <div className="stripe accent-gray" /> */}
       </div>
       {/*<div className="floating-illustration computer"><img src={computer} /></div>*/}
       <h1 className="section-heading" id="sub-heading">
