2a0be7dacd5c402a5f9982458376b721bd083f86
marcopizarro <marcoinnovates@gmail.com>
2021-02-11 13:45:06 -0800

    Add Logos 2021

--- src/js/sponsors.jsx
+++ src/js/sponsors.jsx
@@ -2,10 +2,32 @@ import React from "react";
 import parachute from "../svg/parachute.svg";
 
 const SPONSORS = {
-  biggest: [],
-  big: [],
-  medium: [],
-  small: [],
+  biggest: [
+    {"link": "https://microsoft.com", "img": require('../sponsor-logos/microsoft.png')},
+    {"link": "https://www.cadence.com/", "img": require('../sponsor-logos/cadence.png')}
+  ],
+  big: [
+    {"link": "https://www.esri.com/en-us/home", "img": require('../sponsor-logos/esri1.png')},
+    {"link": "https://www.citadel.com/", "img": require('../sponsor-logos/citadel.png')}
+  ],
+  medium: [
+    {"link": "https://checkbook.io/", "img": require('../sponsor-logos/checkbook.png')},
+    {"link": "https://atsign.com/", "img": require('../sponsor-logos/thecompany.png')},
+    {"link": "https://human.capital", "img": require('../sponsor-logos/hc.png')},
+    {"link": "https://neo.com/", "img": require('../sponsor-logos/neo.png')},
+    {"link": "https://www.deshaw.com/", "img": require('../sponsor-logos/deshaw.png')},
+    {"link": "https://www.stacks.co/", "img": require('../sponsor-logos/blockstack.png')},
+    {"link": "https://human.capital", "img": require('../sponsor-logos/infosys.png')}
+  ],
+  small: [
+    {"link": "https://en.valuenex.com/", "img": require('../sponsor-logos/valuenex.png')},
+    {"link": "https://www.pear.vc/", "img": require('../sponsor-logos/pear.png')},
+    {"link": "https://repl.it/", "img": require('../sponsor-logos/replit.png')},
+    {"link": "https://zoom.us//", "img": require('../sponsor-logos/zoom.png')},
+    {"link": "https://ohyay.co/", "img": require('../sponsor-logos/ohyay.png')},
+    {"link": "https://www.nvidia.com/", "img": require('../sponsor-logos/nvidia.png')},
+    {"link": "https://cloud.google.com/", "img": require('../sponsor-logos/GoogleCloud.png')},
+  ],
   smallest: [],
   tiny: [],
 };
@@ -30,22 +52,22 @@ function Sponsors() {
       </div>
       <div className="floating-illustration parachute"><img src={parachute} /></div>
       <h1 className="section-heading" id="sub-heading">
-        sponsors coming soon
+        sponsors
       </h1>
-      {/*
+      
       {_makeSponsors("biggest")}
       {_makeSponsors("big")}
       {_makeSponsors("medium")}
       {_makeSponsors("small")}
-      {_makeSponsors("smallest")}
-      {_makeSponsors("tiny")}
-      */}
-      <a
+      {/* {_makeSponsors("smallest")}
+      {_makeSponsors("tiny")} */}
+     
+      {/* <a
         href="mailto:hello@treehacks.com?Subject=Sponsorship%20Interest"
         className="apply-button"
       >
         become a sponsor!
-      </a>
+      </a> */}
     </div>
   );
 }
