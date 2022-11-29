0f75c9b555442723bbcda00d3af3fc6d68a77c83
sathviknallamalli <sathviknallamalli@gmail.com>
2022-11-29 15:46:42 -0800

    copied over

--- src/favicons/favicons.js
+++
@@ -1,7 +0,0 @@
-const faviconsContext = require.context(
-  '!!file-loader?name=assets/favicons/favicon.ico!.',
-  true,
-  /\.(svg|png|ico|xml|json)$/
-);
-
-faviconsContext.keys().forEach(faviconsContext);
--- src/index.js
+++ src/index.js
@@ -1,98 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Navbar from "./js/navbar.jsx";
-import Home from "./js/home.jsx";
-import Challenges from "./js/challenges.jsx";
-import About from "./js/about.jsx";
-import Application from "./js/application.jsx";
-import Projects from "./js/projects.jsx";
-import FAQ from "./js/faq.jsx";
-import Sponsors from "./js/sponsors.jsx";
-import Why from "./js/why.jsx";
-import Footer from "./js/footer.jsx";
-// import Team from "./js/team.jsx";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import "./favicons/favicons";
-import "./index.scss";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-function Main() {
-  return (
-    <div>
-      <Navbar />
-      <Home />
-      <About />
-      <Why />
-      <Challenges />
-      {/*<Application />*/}
-      <Projects />
-      <FAQ />
-      <Sponsors />
-      {/* <Footer /> */}
-      {/* <Team /> */}
-    </div>
-  );
-}
-
-function CustomRedirect({ from, to }) {
-  return (
-    <Route
-      exact
-      path={from}
-      component={() => {
-        window.location = to;
-        return null;
-      }}
-    />
-  );
-}
-
-function App() {
-  return (
-    <Router>
-      <Switch>
-        {/* <CustomRedirect
-          from="/slack"
-          to="https://join.slack.com/t/treehacks-2019/shared_invite/enQtNTE5ODY0MDI2NjQ3LWI2YjhlYTU3OTE1ODcyMTdmZWYzYWVjY2NjNzQ2YTBiZDY5YWYwYzhkMDhlZTBmNmNmMzMzZWYxM2QwMTMxMmI"
-        />
-        <CustomRedirect
-          from="/volunteer"
-          to="https://goo.gl/forms/pZURr2kMSKwuFar42"
-        />
-        <CustomRedirect
-          from="/join"
-          to="https://treehacks.typeform.com/to/rK08ej"
-        /> */}
-        <CustomRedirect
-          from="/match"
-          to="https://treehacks.typeform.com/to/ZmcLkzFq"
-        />
-        <CustomRedirect
-          from="/judge"
-          to="https://treehacks.typeform.com/to/QyaH8I"
-        />
-        <CustomRedirect
-          from="/mentor"
-          to="https://treehacks.typeform.com/to/OSt3yo"
-        />
-        <CustomRedirect
-          from="/code-of-conduct"
-          to="https://github.com/TreeHacks/policies/blob/master/code-of-conduct.md"
-        />
-        <CustomRedirect
-          from="/privacy-policy"
-          to="https://github.com/TreeHacks/policies/blob/master/privacy-policy.md"
-        />
-        <Route exact path="/" component={Main} />
-        <Redirect to="/" />
-      </Switch>
-    </Router>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
