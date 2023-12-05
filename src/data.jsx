b2b02a126aabc817a5e2f52657f7daa7f5aed58e
Ananya Gupta <93958307+guananya@users.noreply.github.com>
2023-12-04 17:30:39 -0800

    Add files via upload

---
+++ src/data.jsx
@@ -0,0 +1,65 @@
+// data.js
+const dropdownData = [
+    {
+      title: "Applying",
+      content: (
+        <p>
+          The application deadline is December 11th, 11:59 PST for non-Stanford students, and
+          December 30th, 11:59 PST for Stanford students. Go to our application portal today 
+          to apply- we can't wait to learn more about you! Note that we accept 
+          people individually and not as whole teams. We look for a mix of beginners 
+          and more experienced people- so even if you're new to the space, apply apply apply! 
+
+          *Stanford students are auto-accepted but need to RSVP by applying. 
+        </p>
+      ),
+    },
+    {
+        title: "Transportation",
+        content: (
+          <p>
+            At TreeHacks we're committed to making transportation as convenient 
+            as possible for you. For select schools in California like UC Berkeley 
+            and USC, bus transportation to and from the hackathon will be provided. 
+          </p>
+        ),
+      },
+      {
+        title: "Hacking",
+        content: (
+          <p>
+            Hacking at TreeHacks is unique. With silicon valley right 
+            at our doorstep, we've got mentors from several companies and some 
+            awesome tech for you to tinker with. We've also got a ton of  workshops, 
+            hackpacks,
+          </p>
+        ),
+      },
+      {
+        title: "Learning",
+        content: (
+          <p>
+            Hacking at TreeHacks is unique. With silicon valley right 
+            at our doorstep, we've got mentors from several companies and some 
+            awesome tech for you to tinker with. We've also got a ton of  workshops, 
+            hackpacks,
+          </p>
+        ),
+      },
+      {
+        title: "Events",
+        content: (
+          <p>
+            At TreeHacks, learning is just as exciting as the hacking itself! 
+            Alongside a plethora of fun activities, we provide numerous technical 
+            workshops and hackpacks to enhance your skills both before and during 
+            the event. These workshops cover a range of topics, from 
+            beginner-friendly introductions to advanced technology dives to 
+            understandin
+          </p>
+        ),
+      },
+    // ... other dropdown items
+  ];
+  export default dropdownData;
+  
