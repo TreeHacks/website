7335641043ed07a8d8615edac1aa36c03771635f
Ananya Gupta <93958307+guananya@users.noreply.github.com>
2023-12-12 11:00:50 -0800

    update countdown to hackathon date

--- src/App.js
+++ src/App.js
@@ -306,7 +306,7 @@ function App() {
               <p className='font-semibold xl:text-white lg:text-white md:text-white text-white mb-1 font-display xl:text-lg lg:text-md text-xs'>February 16th-18th 2024 @ Stanford University</p>
               <div class="h-[2px] bg-gradient-to-r xl:bg-white lg:bg-white md:bg-white bg-white rounded-full mb-4"></div>
             </div> 
-            <Countdown className='xl:text-7xl lg:text-6xl text-8xl mb-4 font-CerealBD text-white' targetDate="2023-12-12T08:00:00Z" />
+            <Countdown className='xl:text-7xl lg:text-6xl text-8xl mb-4 font-CerealBD text-white' targetDate="2024-02-16T08:00:00Z" />
             <h1 className='mb-3 xl:text-3xl lg:text-xl text-md xl:text-white lg:text-white md:text-white text-white font-CerealMd'> left to apply (by Dec 11th)</h1>
             <h4 className='mb-8 xl:text-white lg:text-white md:text-white text-white font-CerealMd'> Stanford students deadline- Dec 30th</h4>
             <a target="_blank" href="https://root.treehacks.com/" class="apply-button">
