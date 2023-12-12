f6690263bf5295fd8de17a83e5847ce78b94b77b
Ananya Gupta <93958307+guananya@users.noreply.github.com>
2023-12-12 11:13:54 -0800

    update to countdown to hackathon

--- src/App.js
+++ src/App.js
@@ -336,7 +336,7 @@ const [lastScrollTop, setLastScrollTop] = useState(0); // Initialize the state
               <p className='font-semibold xl:text-white lg:text-white md:text-white text-white mb-1 font-display xl:text-lg lg:text-md text-xs'>February 16th-18th 2024 @ Stanford University</p>
               <div class="h-[2px] bg-gradient-to-r xl:bg-white lg:bg-white md:bg-white bg-white rounded-full mb-4"></div>
             </div> 
-            <Countdown className='xl:text-7xl lg:text-6xl text-8xl mb-4 font-CerealBD text-white' targetDate="2023-12-16T00:00:00" />
+            <Countdown className='xl:text-7xl lg:text-6xl text-8xl mb-4 font-CerealBD text-white' targetDate="2024-02-16T00:00:00" />
             <h1 className='mb-8 xl:text-3xl lg:text-xl text-md xl:text-white lg:text-white md:text-white text-white font-CerealMd'> left to hack!</h1>
             <a target="_blank" href="https://root.treehacks.com/" class="apply-button">
               <span>Apply to hack!</span>
