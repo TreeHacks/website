c362040c0d19daf3cb77d1d619efd0f3792290cc
Ananya Gupta <93958307+guananya@users.noreply.github.com>
2023-12-12 11:00:20 -0800

    update countdown

--- src/Countdown.jsx
+++ src/Countdown.jsx
@@ -1,15 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
-const calculateTimeLeft = (targetDateUTCString) => {
-  // Convert the target UTC date string to a Date object
-  const targetDateUTC = new Date(targetDateUTCString);
-
-  // Get the current time in UTC
-  const nowUTC = new Date();
-
-  // Calculate the difference in milliseconds
-  const difference = targetDateUTC - nowUTC;
-
+const calculateTimeLeft = (targetDate) => {
+  const difference = +new Date(targetDate) - +new Date();
   let timeLeft = {};
 
   if (difference > 0) {
@@ -19,6 +11,8 @@ const calculateTimeLeft = (targetDateUTCString) => {
       minutes: Math.floor((difference / 1000 / 60) % 60),
       seconds: Math.floor((difference / 1000) % 60),
     };
+  } else {
+    console.error('Target date is either invalid or in the past');
   }
 
   return timeLeft;
@@ -32,12 +26,14 @@ const Countdown = ({ targetDate }) => {
       setTimeLeft(calculateTimeLeft(targetDate));
     }, 1000);
 
-    // Clear timeout if the component is unmounted
     return () => clearTimeout(timer);
   });
 
-  // Destructure the timeLeft object
   const { days, hours, minutes, seconds } = timeLeft;
+
+  if (!days && !hours && !minutes && !seconds) {
+    return <div>Loading...</div>; // or any placeholder
+  }
 
   return (
     <div className="countdown">
@@ -49,4 +45,3 @@ const Countdown = ({ targetDate }) => {
 };
 
 export default Countdown;
-
