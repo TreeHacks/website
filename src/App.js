9f014237f0535954c52072dde21ed8dc09822981
Ananya Gupta <93958307+guananya@users.noreply.github.com>
2024-01-08 18:15:10 -0800

    fix sponsor logos

--- src/App.js
+++ src/App.js
@@ -681,28 +681,28 @@ const [lastScrollTop, setLastScrollTop] = useState(0); // Initialize the state
           </div>
           {/* New Row */}
           <div className='flex lg:flex-row flex-col mb-4'>
-            <div className='sponsorCard lg:w-4/12 w-full lg:mr-4'>
+            <div className='sponsorCard lg:w-3/12 w-full lg:mr-4'>
               <a href="https://www.reflex.dev/" target="_blank">
                 <div className='w-full flex justify-center border-2 rounded-md bg-[#B8CCBA] h-40 p-14 relative'>
                   <img src={Reflex} className="object-contain" />
                 </div>
               </a>
             </div>
-            <div className='sponsorCard lg:w-4/12 w-full lg:mr-4'>
+            <div className='sponsorCard lg:w-3/12 w-full lg:mr-4'>
               <a href="https://canva.com/" target="_blank">
                 <div className='w-full flex justify-center border-2 rounded-md bg-[#B8CCBA] h-40 p-14 relative'>
                   <img src={Canva} className="object-contain" />
                 </div>
               </a>
             </div>
-            <div className='sponsorCard lg:w-4/12 w-full'>
+            <div className='sponsorCard lg:w-3/12 w-full'>
               <a href="https://www.parrot.com/us/drones" target="_blank">
                 <div className='w-full flex justify-center border-2 rounded-md bg-[#B8CCBA] h-40 p-10 relative'>
                   <img src={Parrot} className="object-contain" />
                 </div>
               </a>
             </div>
-            <div className='sponsorCard lg:w-4/12 w-full'>
+            <div className='sponsorCard lg:w-3/12 w-full'>
               <a href="https://www.hudsonrivertrading.com/" target="_blank">
                 <div className='w-full flex justify-center border-2 rounded-md bg-[#B8CCBA] h-40 p-10 relative'>
                   <img src={HRT} className="object-contain" />
