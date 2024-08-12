fe452e7030a3874fa65bfb86031c6e5aa850b7ae
Emily Han <emilyhyf@emilys-mbp-2.lan>
2024-08-11 21:17:59 -0700

    Changed parallax direction + added About section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+/// <reference types="vite-plugin-svgr/client" />
 import React from 'react';
 import ParallaxSection from './components/Parallax/Parallax';
-import NavigationBar from './components/NavBar/NavBar';
+import AboutSection from './components/About/about';
 import './App.css'
 
 const App: React.FC = () => {
   return (
-    <div className="home">
-      <div className="section1"><NavigationBar /></div>
-      <div className="section2"><ParallaxSection /></div>
+    <div>
+      <ParallaxSection />
     </div>
   );
 }
--- src/components/Parallax/Parallax.tsx
+++ src/components/Parallax/Parallax.tsx
@@ -5,42 +5,55 @@ import Layer3 from "./layer3.svg?react";
 import Layer4 from "./layer4.svg?react";
 import Layer5 from "./layer5.svg?react";
 import Layer6 from "./layer6.svg?react";
+import NavigationBar from "../NavBar/NavBar";
 import "./Parallax.css";
+import About from "./about.svg?react";
 import AboutSection from "../About/about";
 
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 const ParallaxSection: React.FC = () => {
+
   return (
     <div className="parallax">
-      <Parallax pages={7} className='animation'>
-        <ParallaxLayer offset={0} speed={0.25}>
-             <div id='titles'>
+      <Parallax pages={2} className='animation'>
+
+        <ParallaxLayer offset={0} speed={0.7}>
+          <div className="animation_layer parallax" id="layer6"><Layer6 className="animation_image"/></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.55}>
+          <div className="animation_layer parallax" id="layer5"><Layer5 className="animation_image"/></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.5}>
+          <div className="animation_layer parallax" id="layer4"><Layer4 className="animation_image"/></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div className="animation_layer parallax" id="layer3"><Layer3 className="animation_image"/></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0}>
+          <div className="animation_layer parallax" id="layer2"><Layer2 className='animation_image'/></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0}>
+          <div className="animation_layer parallax" id="layer1"> <Layer1 className="animation_image"/></div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={0} speed={0}>
+          <NavigationBar />
+          <div id='titles'>
               <h1>TREEHACKS</h1>
               <h2>Feb 14-16, 2025</h2>
             </div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.25}>
-          <div className="animation_layer parallax" id="layer6"><Layer6 className="animation_image"/></div>
+
+        <ParallaxLayer offset={1} speed={0}>
+          <div className='animation_layer parallax' id='about'> <About className="animation_image"/></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.50}>
-          <div className="animation_layer parallax" id="layer5"><Layer5 className="animation_image"/></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.75}>
-          <div className="animation_layer parallax" id="layer4"><Layer4 className="animation_image"/></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={1}>
-          <div className="animation_layer parallax" id="layer3"><Layer3 className="animation_image"/></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={1.25}>
-          <div className="animation_layer parallax" id="layer2"><Layer2 className='animation_image'/></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={1.5}>
-          <div className="animation_layer parallax" id="layer1"> <Layer1 className="animation_image"/></div>
-        </ParallaxLayer>
-        {/* <ParallaxLayer offset={1} speed={1.5}>
-          <AboutSection />
-        </ParallaxLayer> */}
+
       </Parallax>
     </div>
   );
