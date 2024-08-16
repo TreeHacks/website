6f9a123130c862f4468b341ae24a79e618865e59
Connor Fogarty <connorflynnfogarty@gmail.com>
2024-08-16 02:16:16 -0500

    Use `react-scroll-parallax` for parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 /// <reference types="vite-plugin-svgr/client" />
 import React from 'react';
+import { ParallaxProvider } from 'react-scroll-parallax';
 import ParallaxSection from './components/Parallax/Parallax';
 import NavigationBar from './components/NavBar/NavBar';
-import './App.css'
+import AboutSection from './components/About/about';
+import Layer1 from "./components/Parallax/layer1.svg?react";
+import About from "./components/About/about.svg?react";
 
 const App: React.FC = () => {
   return (
-    <div style={{ width: '100%' }}>
+    <div>
       <NavigationBar />
-      <ParallaxSection />
+
+      <ParallaxProvider>
+        <ParallaxSection />
+      </ParallaxProvider>
+
+      <div>
+        <Layer1 width="100%" height="auto" display="block" />
+        <About width="100%" height="auto" display="block" />
+        <AboutSection />
+      </div>
     </div>
   );
 }
