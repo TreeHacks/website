2b3b93bd2765c75f748fd2032b7f75ebb3615b22
Emily Han <emilyhyf@emilys-mbp-2.lan>
2024-08-17 06:32:53 -0700

    Fixed navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -9,8 +9,9 @@ const NavigationBar: React.FC = () => {
         <li className="navbar-sections"><ScrollLink to="section1" smooth={true} duration={500}>Home</ScrollLink></li>
         <li className="navbar-sections"><ScrollLink to="section2" smooth={true} duration={500}>About</ScrollLink></li>
         <li className="navbar-sections"><ScrollLink to="section3" smooth={true} duration={500}>Tracks</ScrollLink></li>
-        <li className="navbar-sections"><ScrollLink to="section4" smooth={true} duration={500}>Sponsors</ScrollLink></li>
-        <li className="navbar-sections"><ScrollLink to="section5" smooth={true} duration={500}>FAQs</ScrollLink></li>
+        <li className="navbar-sections"><ScrollLink to="section4" smooth={true} duration={500}>Speakers</ScrollLink></li>
+        <li className="navbar-sections"><ScrollLink to="section5" smooth={true} duration={500}>Sponsors</ScrollLink></li>
+        <li className="navbar-sections"><ScrollLink to="section6" smooth={true} duration={500}>FAQs</ScrollLink></li>
         <li className="apply-button">
             <ScrollLink to="section6" smooth={true} duration={500}>Apply</ScrollLink>
         </li>
