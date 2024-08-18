2a262a0aa02a3d2f1f0130383ad222e988b8ebdb
Connor Fogarty <connorflynnfogarty@gmail.com>
2024-08-18 13:43:07 -0500

    Add link to root in navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,14 +6,26 @@ const NavigationBar: React.FC = () => {
   return (
     <nav className="navbar">
       <ul>
-        <li className="navbar-sections"><ScrollLink to="section1" smooth={true} duration={500}>Home</ScrollLink></li>
-        <li className="navbar-sections"><ScrollLink to="section2" smooth={true} duration={500}>About</ScrollLink></li>
-        <li className="navbar-sections"><ScrollLink to="section3" smooth={true} duration={500}>Tracks</ScrollLink></li>
-        <li className="navbar-sections"><ScrollLink to="section4" smooth={true} duration={500}>Speakers</ScrollLink></li>
-        <li className="navbar-sections"><ScrollLink to="section5" smooth={true} duration={500}>Sponsors</ScrollLink></li>
-        <li className="navbar-sections"><ScrollLink to="section6" smooth={true} duration={500}>FAQs</ScrollLink></li>
+        <li className="navbar-sections">
+          <ScrollLink to="section1" smooth={true} duration={500}>Home</ScrollLink>
+        </li>
+        <li className="navbar-sections">
+          <ScrollLink to="section2" smooth={true} duration={500}>About</ScrollLink>
+        </li>
+        <li className="navbar-sections">
+          <ScrollLink to="section3" smooth={true} duration={500}>Tracks</ScrollLink>
+        </li>
+        <li className="navbar-sections">
+          <ScrollLink to="section4" smooth={true} duration={500}>Speakers</ScrollLink>
+        </li>
+        <li className="navbar-sections">
+          <ScrollLink to="section5" smooth={true} duration={500}>Sponsors</ScrollLink>
+        </li>
+        <li className="navbar-sections">
+          <ScrollLink to="section6" smooth={true} duration={500}>FAQs</ScrollLink>
+        </li>
         <li className="apply-button">
-            <ScrollLink to="section6" smooth={true} duration={500}>Apply</ScrollLink>
+          <a href="https://root.treehacks.com" target="_blank">Apply</a>
         </li>
       </ul>
     </nav>
